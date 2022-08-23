304d9d1941372a4e1ea542459bd6a497c5311cbc
cristiane <cristianesgcris@gmail.com>
2022-08-23 14:30:01 -0300

    Refactoring binary search.

--- introduction/binary_search/BinarySearch.py
+++ introduction/binary_search/BinarySearch.py
@@ -1,6 +1,6 @@
 class BinarySearch:
 
-    def binary_search(self, data, target, low, high):
+    def binary_search_recursive(self, data, target, low, high):
         """
         Binary search implemented recursively.
         Data should be a list of ordered numbers.
@@ -15,6 +15,29 @@ class BinarySearch:
             if target == data[mid]:
                 return True
             elif target < data[mid]:
-                return self.binary_search(data, target, low, mid-1)
+                return self.binary_search_recursive(data, target, low, mid-1)
             else:
-                return self.binary_search(data, target, mid+1, high)
+                return self.binary_search_recursive(data, target, mid+1, high)
+    
+    def binary_search(self, data, target):
+        """
+        Binary search.
+        Data should be a list of ordered numbers.
+        Returns True if target belongs to data, and False otherwise.
+        """
+        low = 0
+        high = len(data) - 1
+        
+        if high == 0:       # Empty list
+            return False
+        
+        while low <= high:
+            mid = (low + high)//2
+            if target == data[mid]:
+                return True
+            elif target < data[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return False
+
